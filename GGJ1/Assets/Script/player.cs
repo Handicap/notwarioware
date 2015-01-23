1f387680ea8c3c8437fb6b7be9c4de2c41b3731f
Pahasusi <holma.matias@gmail.com>
2015-01-23 21:06:29 +0200

    vittu saatana

---
+++ GGJ1/Assets/Script/player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+    public float angle;
+    public float rotationSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            
+
+
+            angle += 400 * Time.deltaTime * rotationSpeed;
+            transform.eulerAngles = new Vector3(0, 0, angle);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            
+
+
+
+            angle -= 400 * Time.deltaTime * rotationSpeed;
+            transform.eulerAngles = new Vector3(0, 0, angle);
+
+
+        }
+
+
+
+	}
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        endgame();
+     
+    }
+
+
+    void endgame()
+    {
+        Destroy(gameObject);
+
+
+    }
+
+
+}
---
+++ GGJ1/Assets/Script/trap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class trap : MonoBehaviour {
+
+    public float Speed = -1f;
+    public int type;
+    static int count;
+
+	// Use this for initialization
+	void Start () {
+
+        type = ++count;
+
+        int rand = UnityEngine.Random.Range(-1, 2);
+
+        transform.position = new Vector3(1.8604f * rand, type * 5 + 20, 0); //= -1.8604f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(0, Speed, 0);
+	}
+}
