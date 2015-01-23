e6a533ce8b8019436dbbec48fd4891a468aa8eaa
Pahasusi <holma.matias@gmail.com>
2015-01-24 00:50:42 +0200

    oihugfasd

---
+++ GGJ1/Assets/Script/golfball.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class golfball : MonoBehaviour {
+    public int invert;
+    public float speed;
+    public float stoppingspeed;
+    public float speedup;
+    public float speeddown;
+    public float speedleft;
+    public float speedright;
+
+	// Use this for initialization
+	void Start () {
+
+        speed = speed * invert;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (speedleft < speed) speedleft += stoppingspeed;
+            else speedleft = speed;
+            //Vector3 position = this.transform.position;
+            //position.x = position.x - speed;
+            //this.transform.position = position;
+        }
+        else
+        {
+            if (0 < speedleft) speedleft = speedleft - stoppingspeed;
+            if (0 > speedleft) speedleft = 0f;
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //speedright = speed;
+            if (speedright < speed) speedright += stoppingspeed;
+            else speedright = speed;
+            //Vector3 position = this.transform.position;
+            //position.x = position.x + speed;
+            //this.transform.position = position;
+        }
+        else
+        {
+            if (0 < speedright) speedright = speedright - stoppingspeed;
+            if (0 > speedright) speedright = 0f;
+
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //speedup = speed;
+            if (speedup < speed) speedup += stoppingspeed;
+            else speedup = speed;
+            //Vector3 position = this.transform.position;
+            //position.y = position.y + speed;
+            //this.transform.position = position;
+        }
+        else
+        {
+            if (0 < speedup) speedup = speedup - stoppingspeed;
+            if (0 > speedup) speedup = 0f;
+
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //speeddown = speed;
+            if (speeddown < speed) speeddown += stoppingspeed;
+            else speeddown = speed;
+            //Vector3 position = this.transform.position;
+            //position.y = position.y - speed;
+            //this.transform.position = position;
+        }
+        else
+        {
+            if (0 < speeddown) speeddown = speeddown - stoppingspeed;
+            if (0 > speeddown) speeddown = 0f;
+        }
+
+        move();
+
+	}
+
+    void move()
+    {
+        Vector3 position = this.transform.position;
+        position.x = position.x - speedleft;
+        this.transform.position = position;
+        position.x = position.x + speedright;
+        this.transform.position = position;
+        position.y = position.y + speedup;
+        this.transform.position = position;
+        position.y = position.y - speeddown;
+        this.transform.position = position;
+
+        
+        
+        
+      
+
+    }
+
+
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.collider.tag == "maali")
+        {
+            win();
+        }
+
+        if (coll.collider.tag == "laita")
+        {
+            lose();
+        }
+
+
+    }
+
+    void win()
+    {
+        Debug.Log("you win");
+
+    }
+
+    void lose()
+    {
+        Debug.Log("you lose");
+
+    }
+
+
+}
