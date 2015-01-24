9b0197991647abf2912c5c4f85bbc0cd6bd81258
Handicap <sambzalgod_32@hotmail.com>
2015-01-25 01:11:50 +0200

    Sheriffipeliä

---
+++ GGJ1/Assets/Script/tvpeli_sheriff.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tvpeli_sheriff : MonoBehaviour {
+
+    public GameObject laukaus;
+    public bool fade = false;
+    public float fadeSpeed = 0.05f;
+    public float timer = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        laukaus.SetActive(false);
+        guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+        guiTexture.color = Color.clear;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer = timer + Time.deltaTime;
+        if (fade)
+        {
+            guiTexture.color = Color.Lerp(guiTexture.color, Color.red, timer);
+        }
+	}
+
+    public void ammu()
+    {
+        fade = true;
+        laukaus.SetActive(true);
+        guiTexture.enabled = true;
+    }
+}
---
+++ GGJ1/Assets/Script/tvpeli_tv.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tvpeli_tv : MonoBehaviour {
+
+    public bool tv_paalla = false;
+    public bool ohjelma = false;
+    public int ohjelmajarjestys = 0;
+    public int nykyinenohjelma;
+
+    public GameObject tvfiltteri;
+    public GameObject[] ohjelmat;
+    public bool[] nahty;
+    public GameObject sheriffi;
+
+    // 0 sheriffchancella tulee sheriffi
+    public int sheriffchance = 1;
+
+    private bool sheriffiarvottu = false;
+
+    public float ajastin = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ajastin = ajastin + Time.deltaTime;
+
+        // telkkari päälle
+        if (!tv_paalla && ajastin > 3)
+        {
+            tv_paalla = true;
+            ajastin = 0;
+            tvfiltteri.SetActive(true);
+        }
+
+
+        //sheriffin kosto
+
+        if (ohjelmajarjestys > 2 && !sheriffiarvottu)
+        {
+            sheriffchance = Random.Range(0, ohjelmat.Length - ohjelmajarjestys+1);
+            sheriffiarvottu = true;
+        }
+
+
+        // ohjelmia tulemaan
+        if ((tv_paalla && ajastin > 1.5f) && !ohjelma)
+        {
+            //SHERIFFI
+            if (sheriffchance == 0)
+            {
+                ohjelma = true;
+                sheriffi.SetActive(true);
+                ajastin = 0;
+            }
+            else
+                // jotain muuta
+            {
+                ohjelma = true;
+                while (nahty[nykyinenohjelma] == true)
+                {
+                    nykyinenohjelma = Random.Range(0, 6);
+                }
+                ohjelmat[nykyinenohjelma].SetActive(true);
+                nahty[nykyinenohjelma] = true;
+                ajastin = 0;
+                ohjelmajarjestys++;
+            }
+
+        }
+
+        if ((ohjelma && ajastin > 3))
+        {
+            ohjelmat[nykyinenohjelma].SetActive(false);
+            ohjelma = false;
+            if (sheriffchance != 0)
+            {
+                sheriffiarvottu = false;
+                ajastin = 0;
+            }
+
+        }
+
+        if (sheriffchance == 0 && ajastin > 1.5f)
+        {
+            tvpeli_sheriff sheriffiskripti = sheriffi.GetComponent<tvpeli_sheriff>();
+            sheriffiskripti.ammu();
+        }
+
+	}
+}
