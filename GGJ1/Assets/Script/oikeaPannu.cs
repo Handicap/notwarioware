a23410bc481369d21387c1ccd8505a54838c1452
Pahasusi <holma.matias@gmail.com>
2015-01-24 14:18:00 +0200

    jumalauta

---
+++ GGJ1/Assets/Script/oikeaPannu.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oikeaPannu : MonoBehaviour {
+    public float angle;
+    public float maxAngle;
+    public float rotationSpeed;
+
+    public float maxOsuu;
+    public float minOsuu;
+
+    public bool pressed;
+    public bool kahvia;
+    public bool osuu;
+
+
+    public GameObject kahviosuu1;
+    public GameObject kahviosuu2;
+    public GameObject kahviohi;
+
+    public GameObject kasi;
+
+    public Vector3 kahvipaikka1;
+    public Vector3 kahvipaikka2;
+    public Vector3 kahvipaikka3;
+
+    public Vector3 sivupaikka;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            pressed = true;
+
+        }
+
+        if (!pressed) return;
+
+        if (angle < maxAngle)
+        {
+            angle += 100 * Time.deltaTime * rotationSpeed;
+            transform.eulerAngles = new Vector3(0, 0, angle);
+
+        }
+        else
+        {
+            if (kasi.transform.position.x < maxOsuu && kasi.transform.position.x > minOsuu) osuu = true;
+            kahviosuu1.SendMessage("uusipaikka", kahvipaikka1);
+            kahviosuu2.SendMessage("uusipaikka", kahvipaikka2);
+            
+            if (osuu) kahviohi.SendMessage("uusipaikka", kahvipaikka3);
+
+
+        }
+
+
+	}
+
+
+
+ 
+}
---
+++ GGJ1/Assets/Script/vana.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vana : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void uusipaikka(Vector3 paikka)
+    {
+
+        this.transform.position = paikka;
+    }
+
+}
