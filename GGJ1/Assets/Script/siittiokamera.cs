1fefa32ec3388fa22832627b1f6d61beed69795c
Pahasusi <holma.matias@gmail.com>
2015-01-25 14:43:42 +0200

    fiils guud män

---
+++ GGJ1/Assets/Script/siittiokamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class siittiokamera : MonoBehaviour {
+    public Transform target;
+    private Vector3 location;
+    public float muutos = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (target == null) return;
+
+        location = this.transform.position;
+        location.x = target.position.x + muutos;
+        this.transform.position = location;
+	}
+}
