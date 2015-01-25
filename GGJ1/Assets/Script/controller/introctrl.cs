e95f17cd75a742eccdbe71ce85aee5cf5467c51f
Handicap <sambzalgod_32@hotmail.com>
2015-01-25 16:07:15 +0200

    Ennen kuin posahtaa huh huh huh

---
+++ GGJ1/Assets/Script/controller/introctrl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class introctrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	StartCoroutine(kentanvaihto());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator kentanvaihto()
+    {
+        yield return new WaitForSeconds(3.5f);
+        Application.LoadLevel(1);
+    }
+}
